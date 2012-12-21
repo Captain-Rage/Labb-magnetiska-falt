05dd869895b11e7b682c21be26bce88d238bf40b
MFserver <togald.nilsson@gmail.com>
2012-12-21 12:29:08 +0100

    Updated func.m, README.md and Data

---
+++ README.md
@@ -0,0 +1,31 @@
+FILER:
+func.m:			Huvudfunktion, den som gör jobbet
+rapport.{tex|pdf}:	Labbrapporten
+Data:			Råa mätdata
+README.md:		Denna fil
+
+Diverse övriga PDF-dokument är labbinstruktioner.
+
+TODO:
+Serie 1, Bz-led:
+Längs Z-axeln:
+ ~ numeriskt beräknade värden
+ # analytiskt beräknade värden
+ # mätvärden
+Längs X-axeln:
+ - numeriskt beräknade värden
+ - mätvärden
+
+Serie2, Bz-led:
+ - mätvärden, borde ha innehållit data längs Bx-led för att generera snygg graf! Men det går ändå. 
+ - Beräknade värden i två dimensioner
+
+Serie3, Bx-led:
+ - numeriskt beräknade värden
+ - mätvärden
+
+Ja, och så skriva rapporten förstås.
+
+Allt markerat med " - " är sådant som ska göras. 
+Allt markerat med " # " är sådant som är gjort. 
+Allt markerat med " ~ " är påbörjat men inte klart. 
--- func.m
+++ func.m
@@ -1,7 +1,20 @@
+% 
+% Usage: func. This function does not take any input arguments. 
+% 
+
+clear all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Ingående parametrar, konstanter, övrigt
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Ingående storheter
 I=1.04;		% Ström, Ampere
 r=0.155;	% Spolens radie, Meter
 N=130;		% Spolens antal lindningar
+mVtoGauss=1/4.92;	% Faktor för omvandling mellan mV och Gauss
 
 % Parametrar för teoretiska beräkningar
 xstep=0.00005;
@@ -10,11 +23,11 @@ xt=[0:xstep:0.2];
 y=0;
 zt=[-0.2:zstep:0.5];
 
-% Mätdataplot längs z-axeln
-% Det här är inte beräknande kod, bara en massa värden definierade som variabler. 
-zm=[-0.20:0.02:0.50];
-Bzminput=[7.1 8.7 10.7 13.0 15.8 18.8 20 24.4 26.3 27 26.3 25.5 22.8 19.7 16.6 13.7 11.4 9.3 7.7 6.3 5.1 4.2 3.5 2.8 2.4 1.9 1.6 1.3 1.1 1 0.8 0.6 0.5 0.3 0.2 0.1];
-Bzm=Bzminput./4.92;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Mätserie nummer 1, Bz
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Numeriskt längs z-axeln
 %dBzn=10^-3*
@@ -24,9 +37,53 @@ A=(r^2)*pi;			% Spolens area, m^2
 d=sqrt(r^2+zt.^2);		% Avstånd från spole till mätpunkt, meter
 Bza=10^-3*(2*I*N*A)./(d.^3);	% Magnetisk fältstyrka [1 Gauss = 10^-4 Tesla]
 
-% z-plot, naturligtvis skitsnygg eftersom jag kodat den //Togald
+% Mätdata längs z-axeln
+% Det här är inte beräknande kod, bara en massa värden definierade som variabler. 
+zm=[-0.20:0.02:0.50];
+Bzm=mVtoGauss.*[7.1 8.7 10.7 13.0 15.8 18.8 20 24.4 26.3 27 26.3 25.5 22.8 19.7 16.6 13.7 11.4 9.3 7.7 6.3 5.1 4.2 3.5 2.8 2.4 1.9 1.6 1.3 1.1 1 0.8 0.6 0.5 0.3 0.2 0.1];
+
+% Plot längs z-axeln
 plot(zm,Bzm,'k',zt,Bza,'g');
 xlabel('Distance from the solenoid center, meters');
 ylabel('Magnetic field strength, Gauss [1 Gauss = Tesla*10^-4]');
 title('Magnetic field strength along the z axis');
 legend('Measured values','Analytically calculated values','Location','NorthEast');
+
+% Numeriskt längs x-axeln
+
+
+% Mätdata längs x-axeln
+% Det här är inte beräknande kod, bara en massa värden definierade som variabler. 
+
+
+% Plot längs x-axeln
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Mätserie nummer 2, aka the meshgrid
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Beräknad fältstyrka, tredimensionellt
+
+
+% Mätdata
+
+
+% Skitsnygg plot
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Mätserie nummer 3, Bx
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Numeriskt längs x-axeln
+
+
+% Mätdata längs x-axeln
+
+
+% Plot längs x-axeln
